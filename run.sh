9ed7a62acb5d9a5ff65127229f4976f225934860
Ueli Bosshard <ueli@lilo.ch>
2015-11-30 12:53:50 +0000

    first working ex1

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 sudo mn --topo=single,6 --mac --controller=remote,ip=127.0.0.1
-# sudo ovs-vsctl set br
+# sudo ovs-vsctl set bridge s1 protocols=OpenFlow13
 # docker run -ti -p 6633:6633 hsrnetwork/ryu bash
 # ryu-manager --verbose ryu/app/simple_switch_13.py
