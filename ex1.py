27d0e35f67f530d2277fb99cbeef904df7f0bee7
Felix Morgner <felix.morgner@gmail.com>
2015-12-05 12:20:34 +0000

    Cleanup code

--- ex1.py
+++ ex1.py
@@ -1,17 +1,14 @@
 from ryu.base import app_manager
 from ryu.controller import ofp_event
-from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
+from ryu.controller.handler import MAIN_DISPATCHER
 from ryu.controller.handler import set_ev_cls
 from ryu.ofproto import ofproto_v1_3
-from ryu.lib.packet import packet
-from ryu.lib.packet import ethernet
-from ryu.lib.packet import ether_types
 
-class SimpleSwitch13(app_manager.RyuApp):
+class Exercise1(app_manager.RyuApp):
     OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
 
     def __init__(self, *args, **kwargs):
-        super(SimpleSwitch13, self).__init__(*args, **kwargs)
+        super(Exercise1, self).__init__(*args, **kwargs)
 
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
     def packet_in(self, ev):
@@ -20,15 +17,13 @@ class SimpleSwitch13(app_manager.RyuApp):
         parser = datapath.ofproto_parser
         in_port = msg.match['in_port']
 
-        pkt = packet.Packet(msg.data)
-        eth = pkt.get_protocols(ethernet.ethernet)[0]
-
         actions = [parser.OFPActionOutput(datapath.ofproto.OFPP_FLOOD)]
-	
+
         data = None
         if msg.buffer_id == datapath.ofproto.OFP_NO_BUFFER:
             data = msg.data
 
         out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
-                                  in_port=msg.match['in_port'], actions=actions, data=data)
+                                  in_port=msg.match['in_port'],
+                                  actions=actions, data=data)
         datapath.send_msg(out)
