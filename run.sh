1174addfc209df0af48e5b0815e35a51b883b734
Ueli Bosshard <ueli@lilo.ch>
2015-12-05 19:58:43 +0100

    path for exN.py changed

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 sudo mn --topo=single,6 --mac --controller=remote,ip=127.0.0.1
 # sudo ovs-vsctl set bridge s1 protocols=OpenFlow13
 # docker run -ti -p 6633:6633 hsrnetwork/ryu bash
-# ryu-manager --verbose ryu/app/ex1.py
-# ryu-manager --verbose ryu/app/ex2.py
-# ryu-manager --verbose ryu/app/ex3.py
-# ryu-manager --verbose ryu/app/ex4.py
+# ryu-manager --verbose ex1.py
+# ryu-manager --verbose ex2.py
+# ryu-manager --verbose ex3.py
+# ryu-manager --verbose ex4.py
