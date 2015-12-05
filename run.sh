f6177dfa741e91879aaa6fac41fd866300dae3b3
Felix Morgner <felix.morgner@gmail.com>
2015-12-05 18:31:16 +0000

    Cleanup for delivery

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 sudo mn --topo=single,6 --mac --controller=remote,ip=127.0.0.1
 # sudo ovs-vsctl set bridge s1 protocols=OpenFlow13
 # docker run -ti -p 6633:6633 hsrnetwork/ryu bash
-# ryu-manager --verbose ryu/app/simple_switch_13.py
 # ryu-manager --verbose ryu/app/ex1.py
 # ryu-manager --verbose ryu/app/ex2.py
 # ryu-manager --verbose ryu/app/ex3.py
+# ryu-manager --verbose ryu/app/ex4.py
